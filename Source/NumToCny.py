1d20210b3e2868f7477a7eae61dc4fd053c95213
SimonCHEN <csbdess@gmail.com>
2022-03-22 11:22:41 +0800

    平滑升级到python3

--- Source/NumToCny.py
+++ Source/NumToCny.py
@@ -6,7 +6,7 @@
 本代码基于 BSD License 授权。
 '''
 
-from cStringIO import StringIO
+from io import StringIO
 from decimal import Decimal
 import math
 
@@ -71,7 +71,7 @@ def _parse_integer(strio, value, zero_count = 0, is_first_section = False):
     ndigits = int(math.floor(math.log10(value))) + 1
     if value < 1000 and not is_first_section:
         zero_count += 1
-    for i in xrange(0, ndigits):
+    for i in range(0, ndigits):
         factor = int(pow(10, ndigits - 1 - i))
         digit = int(value / factor)
         if digit != 0:
